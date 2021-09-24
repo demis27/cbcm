96a72cb934b59e794e65c0718c1e52faaec5ba7c
Kermabon Stéphane <demis27@demis27.net>
2021-09-24 22:15:11 +0200

    Add mongodb docker
    Signed-off-by: Kermabon Stéphane <demis27@demis27.net>

---
+++ backend/comicbook/src/main/java/org/demis27/cbcm/comicbook/ComicBookApplication.java
@@ -0,0 +1,10 @@
+package org.demis27.cbcm.comicbook;
+
+import io.micronaut.runtime.Micronaut;
+
+public class ComicBookApplication {
+
+    public static void main(String[] args) {
+        Micronaut.run(ComicBookApplication.class, args);
+    }
+}
