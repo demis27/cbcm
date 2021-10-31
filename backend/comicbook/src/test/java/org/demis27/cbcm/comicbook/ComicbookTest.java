daafcd83da1d05e3f10989bceaece16a7971ae54
Kermabon Stéphane <demis27@demis27.net>
2021-10-31 19:41:28 +0100

    Continue first controller/service/repository
    Signed-off-by: Kermabon Stéphane <demis27@demis27.net>

--- backend/comicbook/src/test/java/org/demis27/cbcm/comicbook/ComicbookTest.java
+++ backend/comicbook/src/test/java/org/demis27/cbcm/comicbook/ComicbookTest.java
@@ -1,12 +1,14 @@
 package org.demis27.cbcm.comicbook;
 
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
 import io.micronaut.test.annotation.MockBean;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
-import reactor.test.StepVerifier;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -17,16 +19,18 @@ class ComicbookTest {
     ComicBookRepository repository;
 
     @Inject
-    ComicBookService service;
-
-    @Test
-    void emptyListe() throws Exception {
-        when(repository.list()).thenReturn(Flux.empty());
-        StepVerifier.create(service.getAll()).expectNextCount(0).verifyComplete();
-    }
+    @Client("/")
+    HttpClient client;
 
     @MockBean(MongoDBComicBookRepository.class)
     ComicBookRepository repository() {
         return mock(ComicBookRepository.class);
     }
+
+    @Test
+    void emptyList() throws Exception {
+        when(repository.list()).thenReturn(Flux.empty());
+        assertEquals(client.toBlocking().exchange("/api/v1/comicbooks").getStatus().getCode(), 200);
+    }
+
 }
